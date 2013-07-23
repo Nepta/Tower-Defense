2c7a2ef89ec64064f1d54ae4942a8dc86cc4fbb7
Nepta <nepta23@gmail.com>
2013-07-23 05:00:07 +0200

    displayed the map

---
+++ map.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+#include "map.h"
+/*
+ Authors: Nepta
+ Copyright © 2013
+ You should have received a copy of the
+ GNU General Public License along with this program.
+ If not, see  <http://www.gnu.org/licenses/>.
+*/
+
+int main(){
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Surface *screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE | SDL_ASYNCBLIT | SDL_DOUBLEBUF);
+	SDL_Surface *background = loadMap("resources/forest.png");
+	
+	int time = 100;
+	while(time-- > 0){
+		SDL_BlitSurface(background, NULL, screen, NULL);
+		SDL_Flip(screen);
+	}
+ return 0;
+}
+
+/**
+ * \brief Create a map.
+ *
+ * \param mapName The Path of map's background.
+ * \return The array on the map, in order to use it.
+ */
+SDL_Surface* loadMap(char* mapName){
+	SDL_Surface *background;
+	background = IMG_Load(mapName);
+	if(background == NULL) {
+		printf("failed to load the background map\n");
+		printf("IMG_Load: %s\n", IMG_GetError());
+		exit(-1);
+	}
+	return background;
+}
+
---
+++ map.h
@@ -0,0 +1,9 @@
+#ifndef __map_H__
+#define __map_H__
+
+#include <SDL/SDL.h>
+
+SDL_Surface* loadMap(char* mapName);
+
+#endif /* __map_H__ */
+
