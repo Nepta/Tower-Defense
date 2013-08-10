e0dde09d163668d87b80e60057331304ad491264
Nepta <nepta23@gmail.com>
2013-08-10 19:21:29 +0200

    added inputInterface doc

--- inputInterface.h
+++ inputInterface.h
@@ -3,13 +3,17 @@
 
 #include "menu.h"
 
-extern const int mapWidth;
-extern const int menuWidth;
-
+/**
+ * Container for interface in the game (menu, hud ...)
+ */
 typedef struct{
-	Menu *menu;
+	Menu *menu;	//!< the main menu on the right (for now)
 }Interface;
 
+/**
+ * get mouse position and give it to an interface
+ * \param interfaces container that contain all the interface
+ */
 int pollMouseClick(Interface interfaces);
 
 #endif /* __inputInterface_H__ */
