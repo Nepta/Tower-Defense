8f36730308411fc05bae34c4c9d0189156f60bac
Nepta <nepta23@gmail.com>
2013-08-14 22:38:32 +0200

    added description file for initialisation of data structure

---
+++ resources/data.js
@@ -0,0 +1,9 @@
+{
+"towers":[
+	{"location":"resources/tower.png" ,
+	 "description":"a basic level 1 tower"},
+	
+	{"location":"resources/candy_cane.png",
+	"description":"a big colorful candy cane"}
+]
+}
