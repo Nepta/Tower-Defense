0e8690f5bd65446ed63d8b6ae3032049163da80b
Nepta <nepta23@gmail.com>
2013-08-18 16:56:59 +0200

    added doc for field interface

--- towerField.h
+++ towerField.h
@@ -8,8 +8,19 @@ typedef struct{
 //	SDL_Surface *background;
 }FieldInterface;
 
+/**
+ * create an interface to hang up event in the main field
+ * \return interface to the field
+ */
 FieldInterface* createFieldInterface();
-void clickOnScreen(FieldInterface*, int x,int y);
+
+/**
+ * callback function which is called when we click on the main field
+ * \param field structure defining the field
+ * \param x x position of mouse in the field
+ * \param y y position of mouse in the field
+ */
+void clickOnScreen(FieldInterface* field, int x,int y);
 
 #endif /* __towerField_H__ */
 
