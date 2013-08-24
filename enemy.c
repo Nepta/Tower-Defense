03e38e4f8c8d8abe53afca823b85654d0d9ac349
Nepta <nepta23@gmail.com>
2013-08-24 19:19:49 +0200

    used a more generic method to initialize a swag(group) of enemy

--- enemy.c
+++ enemy.c
@@ -111,44 +111,22 @@ void initEnemyAnimation(Enemy *enemy, TokenIterator *it, char* jsonFile){
 
 EnemySwag* createEnemySwag(Enemy *enemy, int swagSize){
 	EnemySwag *swag = malloc(sizeof (EnemySwag) + swagSize * sizeof (Enemy));
+	swag->swagSize = swagSize;
+	swag->nextSwag = NULL;
 	for(int i=0; i<swagSize; i++){
 		swag->enemy[i].spriteSheet = enemy->spriteSheet;
 		swag->enemy[i].position.x = enemy->position.x + 4*(i+1);
 		swag->enemy[i].position.y = enemy->position.y + 4*(i+1);
 		swag->enemy[i].animationState = STAY;
-		SDL_Rect sprite;
-		swag->enemy[i].animation[DOWN] = malloc(sizeof (EnemyAnimation));
-		swag->enemy[i].animation[DOWN]->nextAnimation = swag->enemy[i].animation[DOWN];
-		sprite.x = 0; sprite.y = 64;
-		sprite.w = 24; sprite.h = 32;
-		swag->enemy[i].animation[DOWN]->animation = sprite;
-	 
-		swag->enemy[i].animation[STAY] = malloc(sizeof (EnemyAnimation));
-		swag->enemy[i].animation[STAY]->nextAnimation = swag->enemy[i].animation[STAY];
-		sprite.x = 0; sprite.y = 64;
-		sprite.w = 24; sprite.h = 32;
-		swag->enemy[i].animation[STAY]->animation = sprite;
-	 
-		swag->enemy[i].animation[UP] = malloc(sizeof (EnemyAnimation));
-		swag->enemy[i].animation[UP]->nextAnimation = swag->enemy[i].animation[UP];
-		sprite.x = 0; sprite.y = 0;
-		sprite.w = 24; sprite.h = 32;
-		swag->enemy[i].animation[UP]->animation = sprite;
-	 
-		swag->enemy[i].animation[LEFT] = malloc(sizeof (EnemyAnimation));
-		swag->enemy[i].animation[LEFT]->nextAnimation = swag->enemy[i].animation[LEFT];
-		sprite.x = 0; sprite.y = 96;
-		sprite.w = 24; sprite.h = 32;
-		swag->enemy[i].animation[LEFT]->animation = sprite;
-	 
-		swag->enemy[i].animation[RIGHT] = malloc(sizeof (EnemyAnimation));
-		swag->enemy[i].animation[RIGHT]->nextAnimation = swag->enemy[i].animation[RIGHT];
-		sprite.x = 0; sprite.y = 32;
-		sprite.w = 24; sprite.h = 32;
-		swag->enemy[i].animation[RIGHT]->animation = sprite;
+
+		swag->enemy[i].spriteSheet = enemy->spriteSheet;
+		swag->enemy[i].animationState = STAY;
+		swag->enemy[i].spriteSize.h = enemy->spriteSize.h;
+		swag->enemy[i].spriteSize.w = enemy->spriteSize.w;
+		for(int ii=0; ii<AnimationStateLenght; ii++){
+			swag->enemy[i].animation[ii] = enemy->animation[ii];	//enemy->animation is never allocated?
+		}
 	}
-	swag->swagSize = swagSize;
-	swag->nextSwag = NULL;
  return swag;
 }
 
